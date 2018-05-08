9c0f712ae742ca8ff76b6e8aa71db9c034eaaee7
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2018-05-08 17:00:43 +0100

    Beginning styles

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import EventListingCard from '../components/EventListingCard'
+import ImageBanner from '../components/ImageBanner/ImageBanner'
 
 export default class Events extends Component {
   state = {
@@ -11,6 +12,11 @@ export default class Events extends Component {
     return (
       <div>
         <h1>Hi from the events page</h1>
+        <ImageBanner
+          imageSrc="https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg"
+          titleText="Celebrate Pride"
+          subtitleText="Events, shows, talks..."
+        />
         <Link to="/">Go back to the homepage</Link>
         {this.state.events.map((event, index) => (
           <EventListingCard
