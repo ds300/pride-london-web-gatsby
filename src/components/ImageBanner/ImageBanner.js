b3a32a568c8d743f35c5522d5f107c82f017b825
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2018-05-08 16:16:51 +0100

    Add base components

---
+++ src/components/ImageBanner/ImageBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const ImageBanner = ({ src }) => (
+  <div>
+
+  </div>
+)
+
+export default ImageBanner
---
+++ src/components/ImageBanner/ImageBanner_spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  shallow
+} from 'enzyme'
+
+import ImageBanner from './ImageBanner'
+
+describe('ImageBanner', () => {
+  it('should render', () => {
+    const wrapper = shallow( < ImageBanner / > )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
