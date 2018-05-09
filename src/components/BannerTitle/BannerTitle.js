ff4828882bfb81757bd0e7f87b9fa8d6808d8c51
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2018-05-09 06:50:40 +0100

    Fix styles and add snapshot test

--- src/components/BannerTitle/BannerTitle.js
+++ src/components/BannerTitle/BannerTitle.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
 const BannerTitle = styled.h1`
-  background-color: white;
+  background-color: grey;
+  display: inline-block;
   color: purple;
   margin-bottom: 0;
   padding: 0 5px;
+
+  &:after {
+    height: 20px;
+    display: inline-block;
+  }
 `
 
 export default BannerTitle
