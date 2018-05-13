ca51b1fe0da4935d07c1e5db25fd237cacd0126b
Jason Lai <jason@cyber-duck.co.uk>
2018-05-13 12:44:13 +0100

    import breakpoints from theme file to grid

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Flex, Box } from 'grid-styled'
 import styled from 'styled-components'
+import theme from '../../theme/theme.js'
 
 const Container = styled(Box)`
-
-  @media (min-width: 375px) {
+  @media (min-width: ${theme.breakpoints[0]}) {
     min-width: 335px;
   }
 
-  @media (min-width: 768px) {
+  @media (min-width: ${theme.breakpoints[1]}) {
     min-width: 668px;
   }
 
-  @media (min-width: 1280px) {
+  @media (min-width: ${theme.breakpoints[2]}) {
     min-width: 1100px;
   }
 
-  @media (min-width: 1440px) {
+  @media (min-width: ${theme.breakpoints[3]}) {
     min-width: 1260px;
-    max-width: 1440px;
+    max-width: ${theme.breakpoints[3]};
   }  
 `
 Container.defaultProps = {
