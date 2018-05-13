1c85280738ba987e80e65ac3718bfd75febaab18
Andrico <andrico1234@yahoo.co.uk>
2018-05-13 21:12:45 +0100

    create static events schedule items

--- src/templates/events/EventScheduleItem.js
+++ src/templates/events/EventScheduleItem.js
@@ -1,27 +1,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const fontStyles = `
+  font-size: 0.8em;
+  letter-spacing: 0.4px;
+  padding: 8px;
+`
+
+const ShowMore = styled.p`
+  border-bottom: 2px solid ${props => props.theme.colors.eucalyptusGreen};
+  color: ${props => props.theme.colors.indigo};
+  font-size: 0.8em;
+  font-weight: bold;
+  letter-spacing: 0.4px;
+  margin: 15px auto;
+  width: fit-content;
+`
+
+const ShowMoreWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+`
+
+const Table = styled.table`
+  text-align: left;
+  width: 100%;
+`
+
+const TableHeader = styled.th`
+  ${fontStyles}
+  background: ${props => props.theme.colors.mediumGrey};
+  color: ${props => props.theme.colors.indigo};
+`
+
+const TableItem = styled.td`
+  ${fontStyles};
+  border-bottom: 1px solid ${props => props.theme.colors.mediumGrey};
+`
+
+const TableItemTime = styled(TableItem)`
+  font-weight: bold;
+  width: 20%;
+`
 
 const EventScheduleItem = props => (
-  <table>
-    <th colSpan="2">{props.title}</th>
-    {props.data.map(item => (
-      <tr key={item.act}>
-        <td>{item.time}</td>
-        <td>{item.act}</td>
-      </tr>
-    ))}
-  </table>
+  <div>
+    <Table>
+      <tbody>
+        <tr>
+          <TableHeader colSpan="2">{props.title}</TableHeader>
+        </tr>
+        {props.data.map(item => (
+          <tr key={item.id}>
+            <TableItemTime>{item.time}</TableItemTime>
+            <TableItem>{item.act}</TableItem>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+    <ShowMoreWrapper>
+      <ShowMore>Show more</ShowMore>
+    </ShowMoreWrapper>
+  </div>
 )
 
 EventScheduleItem.propTypes = {
-  data: PropTypes.object,
+  data: PropTypes.array,
   title: PropTypes.string,
 }
 
 export default EventScheduleItem
-
-// make a table header in here using style components
-// small left column for time
-// large right column for act name
-
-// then get data from contentful
