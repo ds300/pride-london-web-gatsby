f619d087b60d3dde9bee95bb5286613089390266
Natalie Mclaren <natalielmclaren@hotmail.com>
2018-05-25 13:40:10 +0100

    Fix accordion glitch - remove body animation

--- src/templates/help/AccordionWrapper.js
+++ src/templates/help/AccordionWrapper.js
@@ -1,12 +1,13 @@
-
 import styled from 'styled-components'
 
 const AccordionWrapper = styled.div`
   	.accordion__body {
-    	padding-top: 0 !important;
+	    padding-top: 0 !important;
+	    background-color: #efefef !important;
+	    animation: none !important;
   	}
 
-  	.accordion__title {
+	.accordion__title {
 	    padding: 20px !important;
 	    border-radius: 4px;
 	    outline: none;
@@ -18,7 +19,7 @@ const AccordionWrapper = styled.div`
 	      	&:hover {
 	        	color: #2d2f7f;
 	      	}
-    	}
+	    }
   	}
 
 	.accordion__title[aria-selected='true'] {
@@ -27,8 +28,12 @@ const AccordionWrapper = styled.div`
 	    border-right: 2px solid #2cda9d !important;
 	    border-bottom-left-radius: 0 !important;
 	    border-bottom-right-radius: 0 !important;
-	    
-	}
+	    background-color: #ffffff !important;
+
+	    h3 {
+	        color: #2d2f7f;
+	    }
+  	}
 
   	.accordion__body[aria-hidden='false'] {
 	    border-bottom: 2px solid #2cda9d !important;
@@ -36,28 +41,20 @@ const AccordionWrapper = styled.div`
 	    border-right: 2px solid #2cda9d !important;
 	    border-bottom-left-radius: 4px !important;
 	    border-bottom-right-radius: 4px !important;
+	    background-color: #ffffff !important;
 	}
 
-  	.accordion__item + .accordion__item {
+	.accordion__item + .accordion__item {
 	    border-top: none;
 	    margin-top: 20px;
   	}
 
-  	.accordion {
+	.accordion {
 	    border: none;
 	    border-radius: 4px !important;
 	    margin-top: 20px;
 	    margin-bottom: 20px;
-
-	    [aria-selected='true'] {
-	      	background-color: #ffffff !important;
-
-		    h3 {
-		        color: #2d2f7f;
-		    }
-	    }
   	}
 `
-
 
 export default AccordionWrapper
--- src/templates/help/FaqsAccordion.js
+++ src/templates/help/FaqsAccordion.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import {
-  Accordion,
-  AccordionItem,
-  AccordionItemTitle,
-  AccordionItemBody,
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
 } from 'react-accessible-accordion'
+
 import AccordionWrapper from './AccordionWrapper'
 import 'react-accessible-accordion/dist/fancy-example.css'
 
-
-
 const FaqsAccordion = () => (
-  <AccordionWrapper>
-    <Accordion accordion="false">
-      <AccordionItem uuid="0">
-        <AccordionItemTitle>
-          <h3 className="u-position-relative">
-            Question question questionnnn
-            <div class="accordion__arrow" role="presentation"></div>
-          </h3>
-        </AccordionItemTitle>
-        <AccordionItemBody>
-          <p>Here's your answer</p>
-        </AccordionItemBody>
-      </AccordionItem>
-      <AccordionItem uuid="1">
-        <AccordionItemTitle>
-          <h3 className="u-position-relative">
-            Another question!
-            <div class="accordion__arrow" role="presentation"></div>
-          </h3>
-        </AccordionItemTitle>
-        <AccordionItemBody>
-          <p>
-            Gummies chocolate cake marzipan powder pastry cheesecake lollipop.
-          </p>
-        </AccordionItemBody>
-      </AccordionItem>
-    </Accordion>
-  </AccordionWrapper>
+    <AccordionWrapper>
+        <Accordion>
+          <AccordionItem uuid="0">
+            <AccordionItemTitle>
+                <h3 className="u-position-relative">
+                    Question question questionnnn
+                    <div className="accordion__arrow" role="presentation" />
+                </h3>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>Here's your answer</p>
+            </AccordionItemBody>
+          </AccordionItem>
+          <AccordionItem uuid="1">
+            <AccordionItemTitle>
+                <h3 className="u-position-relative">
+                    Another question!
+                    <div className="accordion__arrow" role="presentation" />
+                </h3>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>Gummies chocolate cake marzipan powder pastry cheesecake lollipop. Gummies chocolate cake marzipan powder pastry cheesecake lollipop.</p>
+            </AccordionItemBody>
+          </AccordionItem>
+        </Accordion>
+    </AccordionWrapper>
 )
 
 export default FaqsAccordion
