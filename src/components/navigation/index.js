f135454f5718a83aeb6e569cfe40fa9a0b6e6ad6
kara <kara@moo.com>
2018-05-26 14:06:33 +0100

    Added React-Router-Dom for test, added tests for all navigation bar stuff.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Header from './header'
 
+Header.displayName = 'Header'
+
 class Navigation extends Component {
   constructor(props) {
     super(props)
