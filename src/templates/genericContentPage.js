61dabe1c52c1a08f9549dd4d345a7354f3e36131
Jason <jase_lai@hotmail.com>
2018-06-02 14:51:38 +0100

    fix styled import

--- src/templates/genericContentPage.js
+++ src/templates/genericContentPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 import Helmet from 'react-helmet'
