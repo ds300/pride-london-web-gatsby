e1e54a67f925cc948f623609a3bb5ba1921be84b
David Sheldrick <david@futurice.com>
2018-06-09 10:42:12 +0100

    remove console log

--- src/components/events/eventInfoCard.js
+++ src/components/events/eventInfoCard.js
@@ -146,7 +146,6 @@ export default function EventInfoCard({
     accessibilityOptions,
   },
 }) {
-  console.log(accessibilityOptions)
   return (
     <Wrapper>
       {Boolean(startTime && endTime) && (
