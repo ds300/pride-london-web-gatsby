cba182cf0b3c830e46494069f83a1374d7b997c8
David Sheldrick <david@futurice.com>
2018-06-09 11:06:20 +0100

    put events page at index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,1 @@
-import React, { Fragment } from 'react'
-import Link from 'gatsby-link'
-
-const Home = () => (
-  <Fragment>
-    <h1>Pride!!!!</h1>
-    <Link to="/events/">Events</Link>
-  </Fragment>
-)
-
-export default Home
+module.exports = require('./events')
